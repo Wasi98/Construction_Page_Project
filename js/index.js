cbf8cec697ec0abbbb48bc80feb22a60a6214d26
Wasi Swadhin <wasiswadhin098@gmail.com>
2024-05-03 09:57:50 +0600

    project completed

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+
+    
+
+}
+
+function hideSidebar(){
+    const hidebar = document.querySelector('.sidebar');
+    hidebar.style.display = 'none';
+}
