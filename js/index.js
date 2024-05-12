cfc784c7db5a5fd264c3ab36a609ac960a7c9629
Wasi Swadhin <wasiswadhin098@gmail.com>
2024-05-12 22:42:38 +0600

    sticky nav complete

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
+
 }
 
 function hideSidebar(){
     const hidebar = document.querySelector('.sidebar');
     hidebar.style.display = 'none';
 }
-
-
